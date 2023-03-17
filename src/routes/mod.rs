7129faa1f37240ee1cbcd79ea12fcc574f48a631
Kyle Phillips <kphillips375@yahoo.com>
2023-03-16 23:47:10 -0400

    updated mod file

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,9 @@
-use crate::handlers::{analytics, root};
+use crate::handlers::{analytics, root, users};
 use axum::{routing::get, routing::post, Router};
 
 pub fn routes() -> Router {
     Router::new()
         .route("/", get(root::root_handler))
         .route("/analytics", post(analytics::analytics_handler))
+        .route("/users", get(users::users_handler))
 }
