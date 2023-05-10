2e227d4b8cc8ea9b908c72691dabb41bd7254c37
Kyle Phillips <kphillips375@yahoo.com>
2023-05-10 16:00:19 -0400

    added routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,4 @@
-use crate::handlers::{analytics, root, users};
+use crate::handlers::{analytics, root, users, mlb::mlb_player_profile::{mlb_player_handler}, nba::nba_player_profile::{nba_player_handler}};
 use axum::{routing::get, Router};
 
 
@@ -6,5 +6,7 @@ pub fn routes() -> Router {
     Router::new()
         .route("/", get(root::root_handler))
         .route("/nhl", get(analytics::nhl_handler))
+        .route("/mlb", get(mlb_player_handler))
+        .route("/nba", get(nba_player_handler))
         .route("/users", get(users::users_handler))
 }
