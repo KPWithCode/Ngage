8642f1a6ed520f2f5b5a9b7cc303802e0f32fe56
Kyle Phillips <kphillips375@yahoo.com>
2023-05-16 00:14:44 -0400

    batting average,obp, playername,slg, bestpitch

--- src/handlers/mlb/mlb_player_profile.rs
+++ src/handlers/mlb/mlb_player_profile.rs
@@ -1,12 +1,13 @@
 use axum::{http::Response, http::StatusCode};
 use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::Client;
+use std::collections::HashMap;
 use std::env;
 
 pub async fn mlb_player_handler() -> Response<String> {
     match mlb_player_data().await {
-        Ok(response_body) => {
-            let response = Response::new(response_body);
+        Ok(response_data) => {
+            let response = Response::new(response_data.to_string());
             println!("Response: {:?}", response);
             response
         }
@@ -42,18 +43,48 @@ async fn mlb_player_data() -> Result<String, StatusCode> {
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     let body = String::from_utf8(bytes.to_vec()).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let player_data: serde_json::Value = serde_json::from_str(&body)?;
+    let player_data: serde_json::Value = serde_json::from_str(&body).map_err(|e| StatusCode::INTERNAL_SERVER_ERROR)?;
 
+
+    // Extract the relevant data
+    let player_name = player_data["full_name"].as_str().unwrap_or("");
     let batting_stats = &player_data["seasons"][0]["teams"][0]["statistics"]["batting"];
     let ba = batting_stats["avg"].as_str().unwrap_or("N/A");
     let slg = batting_stats["slg"].as_str().unwrap_or("N/A");
     let obp = batting_stats["obp"].as_str().unwrap_or("N/A");
-    
-    // Construct a new JSON object containing only the relevant data
+    let pitch_stats = &player_data["seasons"][0]["teams"][0]["statistics"]["pitching"]["overall"];
+
+    // Find the pitch type with the lowest strike percentage
+    let mut lowest_strike_pct = f64::MAX;
+    let mut best_pitch_type = "";
+    if let serde_json::Value::Array(pitches) = pitch_stats {
+        let mut pitch_map = HashMap::new();
+        for pitch in pitches {
+            let pitch_type = pitch["pitch_type"].as_str().unwrap_or("");
+            let strikes = pitch["strikes"].as_u64().unwrap_or(0) as f64;
+            let total = pitch["total"].as_u64().unwrap_or(0) as f64;
+            let strike_pct = strikes / total;
+            pitch_map.insert(pitch_type, strike_pct);
+        }
+        if pitch_map.is_empty() {
+            println!("No pitch types found!");
+        } else {
+            for (pitch_type, strike_pct) in pitch_map {
+                if strike_pct < lowest_strike_pct {
+                    lowest_strike_pct = strike_pct;
+                    best_pitch_type = pitch_type;
+                }
+            }
+        }
+    }
+
+    // Construct a new JSON object containing the relevant data
     let response_data = serde_json::json!({
+        "Player Name": player_name,
         "BA": ba,
         "SLG": slg,
         "OBP": obp,
+        "Best Pitch Type": best_pitch_type,
     });
 
     Ok(response_data.to_string())
