efc9b7370f2ffd7e4258479370465ee40c03d6bf
Kyle Phillips <kphillips375@yahoo.com>
2023-05-27 01:20:35 -0400

    mlb in mod

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,8 +1,11 @@
-pub mod analytics;
 pub mod root;
 pub mod users;
+pub mod nhl {
+    pub mod analytics;
+}
 pub mod mlb {
     pub mod mlb_player_profile;
+    pub mod matchup_data;
 }
 pub mod nba {
     pub mod nba_player_profile;
